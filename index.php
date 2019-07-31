c4e259d7253eb7b42fe59aac657306837f49c55c
Anthony David Pulse, Jr <tcp@null.net>
2019-07-31 13:25:00 -0400

    7-31-2019 1:24pm

--- index.php
+++ index.php
@@ -82,31 +82,32 @@ div.horizontal {
   -moz-box-sizing: border-box;
   box-sizing: border-box;
 }
-div.horizontal {
+div.horizontal-mobi {
   display: block;
-  width: 400px;
-  height: 325px;
+  width: 100%;
+  height: 100%;
   overflow: auto;
 }
-div.horizontal .table {
+div.horizontal-mobi .table-mobi {
   display: table;
   table-layout: fixed;
-  width: 329px;
+  width: 100%;
 }
-div.horizontal .table article {
-  width: 400px;
+div.horizontal-mobi .table-mobi article {
+  width: 100%;
   height: 325px;
   display: table-cell;
-  background: #e3e3e3;
+  background: gray;
   vertical-align: middle;
   text-align: center;
     padding-bottom: 17px;
 }
-div.horizontal .table article:nth-child(2n+2) {
+/*
+div.horizontal-mobi .table-mobi article:nth-child(2n+2) {
   background: #d1d1d1;
 }
-
-div.horizontal1 {
+*/
+div.horizontal {
   display: block;
   width: 400px;
   height: 325px;
@@ -115,10 +116,10 @@ div.horizontal1 {
 div.horizontal1 .table1 {
   display: table;
   table-layout: fixed;
-  width: 390px;
+  width: 100%;
 }
 div.horizontal1 .table1 article {
-  width: 400px;
+  width: 500px;
   height: 325px;
   display: table-cell;
   background: #e3e3e3;
@@ -175,43 +176,51 @@ header#pageheader {
   async defer></script>
 </head>
 
-<body style="background-color:lightgray;">
+<body style="background-color:gray;">
 <?php
 $fjs = "javascript:setCookie('addr',document.getElementById('pac-input').value,20);";
 $fjs .= "javascript:setCookie('biz',document.getElementById('biz').value,20);";
 $fjs .= "javascript:setCookie('name',document.getElementById('name').value,20);";
 $fjs .= "javascript:setCookie('type',document.getElementById('type').value,20);";
 ?>
-<div id="pane">
+<section style="background-color:lightgray">
+<div id="horizontal-mobi" style="width:100%;height:70px;position:fixed;background-image:url('blacksand.jpg')">
+<b style="font-size:38;color:gray">Welcome to Diads!</b>
 <form method="GET">
 <input id="name" type="text" name="name" placeholder="Contact Person">
-<input id="biz" type="text" name="biz" placeholder="Name of Business"><br>
+<input id="biz" type="text" name="biz" placeholder="Name of Business">
 <input id="pac-input" name="address" type="text" placeholder="St. No, Street, City, State, Zip, Country">
 <input id="type" type="text" name="type" placeholder="Type of Establishment">
 <button onclick="<?=$fjs?>submit">Hello!</button>
 </form>
 </div>
+</section>
 
 <section>
-<div class="horizontal">
-<div class="table">
-<article style="width:300;border-radius: 10px;border: 5px solid lightgray;">
+<div class="horizontal-mobi" style="z-index:3">
+<div class="table-mobi">
+<article style="height:50%;margin-top:70px;position:fixed;width:300;border-radius: 10px;border: 5px solid lightgray;">
 <div id="map"></div>
 </article>
 </div>
 </div>
 </section>
-<section>
-  <div class="horizontal" style="top_margin:0px;padding-bottom:17px;background-color:lightgray;overflow-x:hidden;overflow-y:none;">
-    <div class="table">
+<section style="background-color:gray">
+  <div class="horizontal-mobi" style="margin-top:400px;padding-bottom:17px;background-color:gray;overflow-x:visible;overflow-y:none;">
+    <div class="table-mobi">
       <article>
 <?php for ($j = 0 ; $j < count($Storename) ; $j++) { ?>
   <section>
-  <div class="horizontal" style="padding-bottom:17px;background-color:lightgray;overflow-x:none;overflow-y:hidden;">
-     <div class="table">
+  <?php $t = '100%';
+    if (count($serial[$j]) <= 3)
+      $t = count($serial[$j]) * 400 . "px";
+  ?>
+    <div class="horizontal-mobi" style="margin-top:<?=($j+1)*400?>;width:<?=$t?>;padding-bottom:17px;background-color:gray;overflow:visible;">
+
+     <div class="table-mobi">
 
 <?php for ($i = 0 ; $i < count($serial[$j]) ; $i++) { ?>
-    <article name="" serial="<?=$serial[$j][$i]?>" class="card" style="width:300;border-radius: 10px;border: 5px solid lightgray;">
+    <article name="" serial="<?=$serial[$j][$i]?>" class="card" style="margin-top:<?=($i)*400?>;background-color:gray;width:400px;border-radius: 10px;border: 5px solid lightgray;">
         <b><i><?=$Storename[$j]?></i></b>
         <hr style="height:1px">
         <b><?=$Storeslogan[$j]?></b><br>
---
+++ pay.php
@@ -0,0 +1,6 @@
+require_once 'PATH_TO_BRAINTREE/lib/Braintree.php';
+
+Braintree_Configuration::environment('sandbox');
+Braintree_Configuration::merchantId('n5bdvsd3ghwffd4k');
+Braintree_Configuration::publicKey('xmg2fvqdxsq3dw2n');
+Braintree_Configuration::privateKey('bc696d30a1bb129f53b1f4a7e306c4a7');
